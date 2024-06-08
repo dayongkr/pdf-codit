02fd4a27e3156dd36def5257072c3457c6cad364
dayongkr <dayongkr@gmail.com>
2024-06-08 19:11:24 +0900

    chore: remove a unused loading component

--- src/app/viewer/page.tsx
+++ src/app/viewer/page.tsx
@@ -39,75 +39,62 @@ export default function Viewerpage() {
 
   return (
     <div className="flex h-full w-full">
-      {pages.length ? (
-        <>
-          <div
-            className="flex w-1/3 flex-col gap-3 overflow-auto p-5 hover:cursor-pointer"
-            ref={previewRef}
+      <div
+        className="flex w-1/3 flex-col gap-3 overflow-auto p-5 hover:cursor-pointer"
+        ref={previewRef}
+      >
+        {pages.map((page, index) => (
+          <Card
+            key={index}
+            className={cn('p-3', currentPage === index + 1 && 'border-primary')}
+            onClick={() => setCurrentPage(index + 1)}
           >
-            {pages.map((page, index) => (
-              <Card
-                key={index}
-                className={cn(
-                  'p-3',
-                  currentPage === index + 1 && 'border-primary'
-                )}
-                onClick={() => setCurrentPage(index + 1)}
-              >
-                <PdfCanvas page={page} preview />
-              </Card>
-            ))}
-          </div>
-          <div className="flex h-full w-full flex-col">
-            <header className="relative flex w-full items-center justify-center gap-3 border-b-2 border-b-secondary p-2">
-              <Button
-                className="absolute left-5"
-                variant="outline"
-                size="icon"
-                onClick={() => router.push('/')}
-              >
-                <HomeIcon />
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => {
-                  decrementPage()
-                  scrollToPreview(previewRef, currentPage - 2)
-                }}
-                disabled={currentPage === 1}
-              >
-                <ArrowLeftIcon />
-              </Button>
-              <span className="text-primary">{currentPage}</span>
-              <span className="text-secondary-foreground opacity-50">
-                {' '}
-                of {pages.length}
-              </span>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={() => {
-                  incrementPage()
-                  scrollToPreview(previewRef, currentPage)
-                }}
-                disabled={currentPage === pages.length}
-              >
-                <ArrowRightIcon />
-              </Button>
-            </header>
-            <div className="flex h-full w-full items-center justify-start overflow-auto p-3">
-              <PdfCanvas page={pages[currentPage - 1]} />
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="flex h-full w-full items-center justify-center">
-          <ShadowInnerIcon className="animate-ping" />
-          <ShadowInnerIcon className="animate-ping delay-75" />
-          <ShadowInnerIcon className="animate-ping delay-150" />
+            <PdfCanvas page={page} preview />
+          </Card>
+        ))}
+      </div>
+      <div className="flex h-full w-full flex-col">
+        <header className="relative flex w-full items-center justify-center gap-3 border-b-2 border-b-secondary p-2">
+          <Button
+            className="absolute left-5"
+            variant="outline"
+            size="icon"
+            onClick={() => router.push('/')}
+          >
+            <HomeIcon />
+          </Button>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => {
+              decrementPage()
+              scrollToPreview(previewRef, currentPage - 2)
+            }}
+            disabled={currentPage === 1}
+          >
+            <ArrowLeftIcon />
+          </Button>
+          <span className="text-primary">{currentPage}</span>
+          <span className="text-secondary-foreground opacity-50">
+            {' '}
+            of {pages.length}
+          </span>
+          <Button
+            variant="outline"
+            size="icon"
+            onClick={() => {
+              incrementPage()
+              scrollToPreview(previewRef, currentPage)
+            }}
+            disabled={currentPage === pages.length}
+          >
+            <ArrowRightIcon />
+          </Button>
+        </header>
+        <div className="flex h-full w-full items-center justify-start overflow-auto p-3">
+          <PdfCanvas page={pages[currentPage - 1]} />
         </div>
-      )}
+      </div>
     </div>
   )
 }
